a8a6952d883107b0fd22873865f98ab2550f0dad
Raveen Panditha <raveenpanditha9@gmail.com>
2021-10-03 00:40:35 +0530

    request page

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -9,10 +9,12 @@ function App() {
   );
 }
 
-29e4784ca562b7726b03b33571f49b3f
+// API Key (v3 auth)
+//29e4784ca562b7726b03b33571f49b3f
 
-https://api.themoviedb.org/3/movie/550?api_key=29e4784ca562b7726b03b33571f49b3f
+// https://api.themoviedb.org/3/movie/550?api_key=29e4784ca562b7726b03b33571f49b3f
 
-eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOWU0Nzg0Y2E1NjJiNzcyNmIwM2IzMzU3MWY0OWIzZiIsInN1YiI6IjYxNThhNGYxZWMxODc5MDA0MmY3NmNjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vPuUG94yEJYioat7dCHepuDnLld_dCvvdQcz4yzsck4
+// API Read Access Token (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOWU0Nzg0Y2E1NjJiNzcyNmIwM2IzMzU3MWY0OWIzZiIsInN1YiI6IjYxNThhNGYxZWMxODc5MDA0MmY3NmNjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vPuUG94yEJYioat7dCHepuDnLld_dCvvdQcz4yzsck4
 
 export default App;
