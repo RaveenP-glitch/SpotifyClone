2074ac5dc6801eea3e6d011a3c9232aa28076c80
Raveen Panditha <raveenpanditha9@gmail.com>
2021-10-03 00:36:28 +0530

    initial commit

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hellow</h1>
+    </div>
+  );
+}
+
+29e4784ca562b7726b03b33571f49b3f
+
+https://api.themoviedb.org/3/movie/550?api_key=29e4784ca562b7726b03b33571f49b3f
+
+eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOWU0Nzg0Y2E1NjJiNzcyNmIwM2IzMzU3MWY0OWIzZiIsInN1YiI6IjYxNThhNGYxZWMxODc5MDA0MmY3NmNjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.vPuUG94yEJYioat7dCHepuDnLld_dCvvdQcz4yzsck4
+
+export default App;
