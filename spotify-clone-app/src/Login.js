0702c9b01cafecfd8a0f4a1666f6c26efd679a7d
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2021-12-15 23:19:23 +0530

    Login component

---
+++ spotify-clone-app/src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div className="Login">
+            <h1>I am the login page.</h1>
+            {/* Spotify logo */}
+            {/* Login with Spotify button */}
+
+        </div>
+    )
+}
+
+export default Login
